27d72ddf04a3246cca4cf5317189fe51d8c44f8b
tgutierrez <tgutierrez@saepc78.ssi-schaefer.local>
2019-04-29 13:38:23 +0200

    example factory Method

---
+++ src/main/Principal.java
@@ -0,0 +1,24 @@
+package main;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Jugador j = JugadorFactory.crearJugador();
+	}
+	
+	static class Jugador{
+		public Jugador() {
+			System.out.println("creando objeto que requere de una " +
+					" dificil configuracion ");			
+		}
+	}
+	
+	static class JugadorFactory{
+		public static Jugador crearJugador() {
+			System.out.println("creando objeto que requiere de varias preparaciones");
+			return new Jugador();
+		}
+	}
+
+}
